91f02871ee586a6b5575ca41f3d7499f5a07f980
lllilllilllilili <hik3562@hufs.ac.kr>
2022-11-14 00:38:38 +0900

    프로젝트 셋팅

---
+++ src/test/java/com/studyroom/StudyroomApplicationTests.java
@@ -0,0 +1,11 @@
+package com.studyroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyroomApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
