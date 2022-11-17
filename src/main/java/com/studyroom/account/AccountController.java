ba899df80fe8321120e235cfdc62cebd27b88de1
lllilllilllilili <hik3562@hufs.ac.kr>
2022-11-18 01:50:05 +0900

    회원 가입 처리 분기 작성

--- src/main/java/com/studyroom/account/AccountController.java
+++ src/main/java/com/studyroom/account/AccountController.java
@@ -2,7 +2,13 @@ package com.studyroom.account;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
 public class AccountController {
@@ -11,5 +17,14 @@ public class AccountController {
     public String signUpForm(Model model) {
         model.addAttribute( new SignUpForm());
         return "account/sign-up";
+    }
+
+    @PostMapping("/sign-up")
+    public String signUpSubmit(@Valid SignUpForm signUpForm, Errors errors) {
+        if (errors.hasErrors()) {
+            return "account/sign-up";
+        }
+        //TODO 회원 가입 처리
+        return "redirect:/";
     }
 }
